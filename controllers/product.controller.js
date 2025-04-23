364e7e26902034a86d0a244a3db26df366355faa
senin <seninsimon002@gmail.com>
2025-04-23 15:10:06 +0530

    backend postman verifying completed

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,32 +3,83 @@ import Product from "../models/product.models.js";
 
 
 
-export const createProduct = async (req , res)=>
-{
+//creating or adding the product
+export const createProduct = async (req, res) => {
+  const product = req.body;
 
-    const { name , price , image } = req.body
+  console.log(product);
 
-    console.log(req.body)
+  if (!product.name || !product.image || !product.price) {
+    return res
+      .status(400)
+      .json({ success: false, message: "please provide all fields" });
+  }
 
-    // if(!product.name || !product.image || !product.price)
-    // {
-    //     return res.status(400).json({ success : false ,  message : "please provide all fields"})
-    // }
+  try {
+    const products = await Product.create(product);
 
-    try {
+    res.status(200).json({
+      success: true,
+      message: "new product is created",
+      data: products,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
+};
 
-        const product = await Product.create({ name , price , image })
-        
 
-        res.status(200).json({success : true , message : "new product is created" , data : product})
-        
 
-        
-    } catch (error) {
 
-        res.status(500).json({success : false , message : "internal server error"})
-         
 
-    }
-}
+//fetching all the products
+export const getProducts = async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    res.status(200).json({
+      success: true,
+      data: allProducts,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
+};
 
+
+///updating the product
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "products are updated ",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
+};
+
+
+
+//deleting the products
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedData = await Product.findByIdAndDelete(id);
+
+    res
+      .status(200)
+      .json({ success: true, message: "product has been deleted" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
+};
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { createProduct } from '../controllers/product.controller.js'
+import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/product.controller.js'
 const router = express.Router()
 
 
 
 router.post('/' , createProduct)
+router.get('/' , getProducts)
+router.put('/:id' , updateProduct)
+router.delete('/:id' , deleteProduct)
+
 
 
 
